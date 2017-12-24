17afa369714a75b38079981a625852f3522ed93a
Ashkan Paleganeh <ashkanpaleganeh@gmail.com>
2017-12-24 20:57:31 +0330

    Add skills contexts.

---
+++ dist/assets/skills.js
@@ -0,0 +1,67 @@
+let skills = {
+    "HTML(5)": {
+        "stars": 4.5
+    },
+    "CSS(3)": {
+        "stars": 4.5,
+        "subs": [
+            "SASS",
+            "LESS",
+            "Stylus",
+            "Bootstrap", 
+            "Semantic UI" 
+        ]
+    }, 
+    "Javascript": {
+        "stars": 4,
+        "subs": [
+            "Ajax",
+            "jQuery",
+            "AngularJS", 
+            "Angular",
+            "React",
+            "Node.js",
+            "Gulp, Webpack",
+            "Leaflet, Open Street Map, Google Map, etc",
+            "Chart.js, Chartist, D3 charts, etc"
+        ]
+    },
+    "PHP": {
+        "stars": 2.5,
+        "subs": [
+            "MVC programming", 
+            "Slim framework",
+            "Restfull web APIs"
+        ]
+    },
+    "Template Engines":{
+        "stars": 4,
+        "subs":[
+            "Pug",
+            "Twig"
+        ]
+    },
+    "Databases": {
+        "stars": 2.5,
+        "subs": [
+            "MySQL", 
+            "MongoDB"
+        ]
+    },
+    "CMSs": {
+        "stars": 2.5,
+        "subs": [
+            "Joomla", 
+            "Wordpress"
+        ]
+    },
+    "Git": {
+        "stars": 3.5
+    },
+    "Linux": {
+        "stars": 2,
+        "subs": [
+            "LPIC I", "LPIC II", "Ubuntu"
+        ]
+    }  
+};

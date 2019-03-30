5c9ae02e03e24aeda6d32d8e728cc93898b3c05c
Ashkan Paleganeh <ashkanpaleganeh@gmail.com>
2019-03-30 22:05:47 +0430

    Add gulpfile and package.json

---
+++ gulpfile.js
@@ -0,0 +1,183 @@
+/* 
+ * Gulp Pure Start (GPS) Copyright © 2017, Nikita Mihalyov <nikita.mihalyov@gmail.com>
+ * ISC Licensed
+ * URL: https://github.com/nmihalyov/gulp-pure-start
+ * URL: https://github.com/gulpjs/gulp/blob/master/docs/recipes/running-task-steps-per-folder.md
+ * v0.7.1
+ */
+
+let source  = './src/',  
+    dist    = './dist/',
+    pages   = ['home'];
+
+// Return list of folders in the dir
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+        .filter(function(file) {
+            return fs.statSync(path.join(dir, file)).isDirectory();
+        });
+}
+
+const gulp         = require('gulp'),
+      sass         = require('gulp-sass'),
+      pug          = require('gulp-pug'),
+      babel        = require('gulp-babel'),
+      sourcemaps   = require('gulp-sourcemaps'),
+      concat       = require('gulp-concat'),
+      uglify       = require('gulp-uglify'),
+      cssnano      = require('gulp-cssnano'),               // CSS minification package
+      rename       = require('gulp-rename'),
+      autoprefixer = require('gulp-autoprefixer'),
+      plumber      = require('gulp-plumber'),               // Prevents the breaking of pipes caused by errors gulp-plugins
+      notify       = require('gulp-notify'),                // Displays notifications
+      eslint       = require('gulp-eslint');                // Service Worker Precache
+
+var fs = require('fs'),
+    path = require('path'),
+    merge = require('merge-stream'),
+    folders = getFolders(source);
+
+// Development SASS task (with sourcemap)
+gulp.task('sass',  () => {
+    let tasks = folders.map(function(folder){
+        return gulp.src(source + folder + '/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: ':nested'
+        }))
+        .on('error', notify.onError({
+            title: 'SASS',
+            message: '<%= error.message %>'
+        }))
+        .pipe(autoprefixer(['last 15 versions', '> 1%'], {cascade: false}))
+        .pipe(cssnano())
+        .pipe(rename(folder + '.min.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(dist + 'css/'));
+    });
+
+    return merge(tasks);
+});
+
+// production SASS task (without sourcemap)
+gulp.task('_sass',  () => {
+    let tasks = folders.map(function(folder){
+        return gulp.src(source + folder + '/*.scss')
+            .pipe(sass())
+            .pipe(autoprefixer(['last 15 versions', '> 1%'], {cascade: false}))
+            .pipe(cssnano())
+            .pipe(rename(folder + '.min.css'))
+            .pipe(gulp.dest(dist + 'css/'));
+    });
+
+    return merge(tasks);
+});
+
+// Компилируем Pug (Jade) в HTML без его минификации
+gulp.task('pug',  () => {
+    let tasks = folders.map(function(folder){
+        return gulp.src(source + folder + '/*.pug')
+            .pipe(pug({
+                pretty: true 
+            }))
+            .on('error', notify.onError({
+                title: 'PUG',
+                message: '<%= error.message %>' 
+            }))
+            .pipe(gulp.dest(dist + 'html/'));
+    });
+
+    return merge(tasks);
+});
+
+gulp.task('_pug',  () => {
+    let tasks = folders.map(function(folder){
+        return gulp.src(source + folder + '/*.pug')
+            .pipe(pug({
+                pretty: true
+            }))
+            .on('error', notify.onError({
+                title: 'PUG',
+                message: '<%= error.message %>'
+            }))
+            .pipe(gulp.dest(dist + 'html/'));
+    });
+
+    return merge(tasks);
+});
+
+//JS-Code Linting
+gulp.task('eslint', () => {
+    return gulp.src([dist + 'js/*.js'])         // all JS files
+    .pipe(eslint({
+        fix: true,                              // if possible correcting the error
+        rules: {
+            'no-undef': 0                       // Do not let unlimited variables (such as global and libraries)
+        },
+        globals: ['$']                          // define global variables (the most common is jQuery)
+    }))
+    .pipe(eslint.format());                     // Displays ESLint messages to the console
+});
+
+gulp.task('scripts', ['eslint'], () => {
+    let tasks = folders.map(function(folder){
+        return gulp.src(source + folder + '/*.js')
+            .pipe(concat(folder + '.min.js'))
+            .pipe(plumber({
+                errorHandler: notify.onError({
+                    title: 'JS',
+                    message: '<%= error.message %>'     //output error message
+                })
+            }))
+            .pipe(sourcemaps.init())                    
+            .pipe(babel({
+                presets: ['env']
+            }))                 // ES6 to ES5
+            .pipe(uglify({
+				mangle: {
+					toplevel: true,
+					reserved: [
+						'toggleMenu',
+                        'downloadAsPic',
+                        'localizeAllContents'
+					]
+				}
+            }))
+            .pipe(sourcemaps.write())                   // sourcemap entry
+            .pipe(gulp.dest(dist + 'js/'));
+    });
+
+    return merge(tasks);
+});
+
+gulp.task('_scripts', ['eslint'], () => {
+    let tasks = folders.map(function(folder){
+        return gulp.src(source + folder + '/*.js')
+            .pipe(concat(folder + '.min.js'))
+            .pipe(babel({
+                presets: ['env']
+            }))
+            .pipe(uglify({
+				mangle: {
+					toplevel: true,
+					reserved: [
+						'toggleMenu',
+						'downloadAsPic',
+                        'localizeAllContents'
+					]
+				}
+			}))
+            .pipe(gulp.dest(dist + 'js/'));
+    });
+
+    return merge(tasks);
+});
+
+gulp.task('default', ['sass', 'pug', 'scripts'], () => {
+    gulp.watch([source + '**/**/*.scss', source + '**/*.scss'], ['sass']);
+    gulp.watch([source + '**/**/*.pug', source + '**/*.pug'], ['pug']);
+    gulp.watch([source + '**/**/*.js', source + '**/*.js'], ['scripts']);
+});
+
+// Build production
+gulp.task('build', ['_sass', '_pug', '_scripts']);

6be2de28902a51793298959a4d6b144d3830b820
ashkan <ashkan@ashkan.ashkan>
2020-05-02 00:18:01 +0430

    Fix side menu problems

--- src/common/side-menu.js
+++ src/common/side-menu.js
@@ -1,14 +1,33 @@
 
-function toggleMenu(action){
+function toggleMenu(){
     let sideMenu = document.getElementsByClassName('side-menu');
-    if(sideMenu.length > 0)
+    if(sideMenu.length > 0){
         sideMenu[0].classList.toggle('width-220px');
-    
-    let sideMenu2 = document.getElementsByClassName('first-page-side-menu');
-    if(sideMenu2.length > 0)
-        sideMenu2[0].classList.toggle('width-220px');
+    }
 
     let firstPageOpenMenuBtn = document.getElementsByClassName('first-page-open-menu');
     if(firstPageOpenMenuBtn.length > 0)
         firstPageOpenMenuBtn[0].classList.toggle('width-220px');
 }
+
+
+(function sideMenu() {
+    document.querySelector('span#open-menu').addEventListener('click', function(){
+        toggleMenu();
+    });
+        
+    document.addEventListener('click',function(event){
+        var sideMenu = document.getElementsByClassName("side-menu");
+
+        if(sideMenu.length > 0) {
+            if( 
+                !event.target.classList.contains('icon-bars') &&
+                event.target.id != 'open-menu' &&
+                !event.target.classList.contains('first-page-open-menu') &&
+                sideMenu[0].classList.contains('width-220px') &&
+                !sideMenu[0].contains(event.target)){
+                    toggleMenu();
+            }       
+        }
+    })
+})();

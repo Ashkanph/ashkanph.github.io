b71389fa9dfaa8e1eb374e804f6e893fefa22850
ashkanph <ashkanpaleganeh@gmail.com>
2024-06-02 12:41:05 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-acfd7122c966ecbdf43e.js"
+    "url": "webpack-runtime-1a791cab590578b86e68.js"
   },
   {
     "url": "framework-533ee537080cfe4d47f3.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8cab6ce9a73da8cf3b9c1e7315db608d"
+    "revision": "35235991dcbb413d2e16cbc4c6bad4d9"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
